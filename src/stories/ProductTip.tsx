3f417fa0b4fe443d970992a56a04bf70916a5738
Thomas May <thomas.may@produktmacher.com>
2023-08-02 20:13:31 +0200

    Setup Prettier

--- src/stories/ProductTip.tsx
+++ src/stories/ProductTip.tsx
@@ -16,24 +16,24 @@ export const ProductTip = ({
         {...props}
         className="tw-rounded-lg tw-border tw-border-[#E5E5E5] tw-p-4 md:tw-p-6 lg:tw-col-span-8 lg:tw-col-start-3"
       >
-        <figure className="tw-relative tw-flex tw-justify-center tw-items-center tw-gap-x-4 md:tw-gap-x-6">
+        <figure className="tw-relative tw-flex tw-items-center tw-justify-center tw-gap-x-4 md:tw-gap-x-6">
           <img
             src="/avatar.png"
-            className="tw-w-14 tw-h-14 md:tw-w-[110px] md:tw-h-[110px] tw-rounded-full tw-bg-gray-100 tw-flex-shrink-0"
+            className="tw-h-14 tw-w-14 tw-flex-shrink-0 tw-rounded-full tw-bg-gray-100 md:tw-h-[110px] md:tw-w-[110px]"
           />
           <figcaption className="tw-w-full">
-            <p className="tw-text-sm tw-text-black tw-font-body">
+            <p className="tw-font-body tw-text-sm tw-text-black">
               Der ADAC Produkt-Tipp von Dr. E
             </p>
-            <p className="tw-hidden md:tw-block md:tw-mt-2 tw-text-headline  tw-text-black tw-font-body">
+            <p className="tw-hidden tw-font-body tw-text-headline tw-text-black  md:tw-mt-2 md:tw-block">
               {headline}
             </p>
           </figcaption>
         </figure>
-        <p className="md:tw-hidden tw-mt-4 tw-text-headline tw-text-black tw-font-body">
+        <p className="tw-mt-4 tw-font-body tw-text-headline tw-text-black md:tw-hidden">
           {headline}
         </p>
-        <p className="tw-mt-4 md:tw-mt-6 tw-text-quote tw-italic  tw-text-black tw-font-body">
+        <p className="tw-mt-4 tw-font-body tw-text-quote tw-italic  tw-text-black md:tw-mt-6">
           "{children}"
         </p>
       </blockquote>
